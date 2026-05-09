79fd6e0747f2d82353bd6551e9f253bd26323089
marcinoktawian <marcin.oktawian@gmail.com>
2026-05-09 10:36:44 +0200

    fix: use power button held time for shutdown logic

--- src/main.cpp
+++ src/main.cpp
@@ -581,7 +581,7 @@ void loop() {
     return;
   }
 
-  if (gpio.isPressed(HalGPIO::BTN_POWER) && gpio.getHeldTime() > SETTINGS.getPowerButtonDuration()) {
+  if (gpio.isPressed(HalGPIO::BTN_POWER) && gpio.getPowerButtonHeldTime() > SETTINGS.getPowerButtonDuration()) {
     // If the screenshot combination is potentially being pressed, don't sleep
     if (gpio.isPressed(HalGPIO::BTN_DOWN)) {
       return;
